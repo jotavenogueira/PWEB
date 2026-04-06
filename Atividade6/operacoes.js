b65620c4ca8962c60fa50503103446c3ce353b10
João Vitor Nogueira Lopes <119711989+jotavenogueira@users.noreply.github.com>
2026-04-06 11:48:05 -0300

    adicionar arquivos da atividade 6

---
+++ Atividade6/operacoes.js
@@ -0,0 +1,13 @@
+let a = parseFloat(prompt("Digite o primeiro numero"));
+let b = parseFloat(prompt("Digite o segundo numero"));
+
+let soma = a + b;
+let sub = a - b;
+let mult = a * b;
+let div = a / b;
+
+alert("Soma: " + soma +
+    "\nSubtração: " + sub + 
+    "\nMultiplicação: " + mult +
+    "\nDivisão: " + div
+)
